722a4e495846e0b577dc1c4077302ae658752735
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-04-21 17:28:23 +0200

    js

--- main.js
+++ main.js
@@ -11,3 +11,8 @@ window.addEventListener('scroll', () => {
 
 const faqs = document.querySelectorAll('.faq');
 
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+    })
+})
